2dbb4ec6472765c83eed67b18c92ec945078192a
eshans <eshans2000@gmail.com>
2023-08-02 18:30:31 +0530

    add toxicity detection

--- frontend/src/resources/category/create.js
+++ frontend/src/resources/category/create.js
@@ -1,15 +1,22 @@
-import * as React from 'react';
-import { Create, SimpleForm, TextInput, DateInput, required ,ReferenceInput, AutocompleteInput} from 'react-admin';
+import * as React from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  required,
+  ReferenceInput,
+  AutocompleteInput,
+} from "react-admin";
+import ToxicityDetectedInput from "../../components/ToxicityDetectedInput";
 
 export const CategoryCreate = () => (
-    <Create>
-        <SimpleForm>
-            
-            <TextInput source="name"/>
-            <ReferenceInput source="parent" reference="categories" >
-            <AutocompleteInput optionText="name" />
-            </ReferenceInput>
-            
-        </SimpleForm>
-    </Create>
+  <Create>
+    <SimpleForm>
+      <ToxicityDetectedInput source="name" validate={required()} />
+      <ReferenceInput source="parent" reference="categories">
+        <AutocompleteInput optionText="name" />
+      </ReferenceInput>
+    </SimpleForm>
+  </Create>
 );
