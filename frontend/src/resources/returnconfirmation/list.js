f9f64b525b661f01712c7d4254f87042ef27cd33
eshans <eshans2000@gmail.com>
2023-08-05 02:02:42 +0530

    return confirmation accept and decline

--- frontend/src/resources/returnconfirmation/list.js
+++ frontend/src/resources/returnconfirmation/list.js
@@ -5,17 +5,65 @@ import {
   DateField,
   BooleanField,
   ReferenceField,
-} from 'react-admin';
+  useCreate,
+  useUpdate,
+  useDelete,
+  FunctionField,
+} from "react-admin";
+import Button from "@mui/material/Button";
 
-export const ReturnConfirmationList = () => (
-  <List>
-    <Datagrid>
-      <TextField source='borrow' />
-      <BooleanField source='returned' />
-      <BooleanField source='received' />
-      <DateField source='lent_date' />
-      <DateField source='received_date' />
-      <ReferenceField source='parent' reference='returnconfirmations' />
-    </Datagrid>
-  </List>
-);
+const ConfirmButton = (record) => {
+  console.log(record);
+
+  const [update, { isLoading, error }] = useUpdate();
+
+  const onConfirm = () => {
+    console.log("update");
+    //create returnconfirmation
+    update("returnconfirmations", {
+      id: record.id,
+      data: { received: true, received_date: new Date() },
+      previousData: record,
+    });
+  };
+
+  return (
+    <Button variant="contained" onClick={onConfirm}>
+      Confirm
+    </Button>
+  );
+};
+
+const DeclineButton = (record) => {
+  console.log(record);
+
+  const [deleteOne, { data, isLoading }] = useDelete();
+
+  const onDecline = () => {
+    deleteOne("returnconfirmations", {
+      id: record.id,
+    });
+  };
+
+  return (
+    <Button variant="contained" color="error" onClick={onDecline}>
+      Decline
+    </Button>
+  );
+};
+
+export const ReturnConfirmationList = () => {
+  return (
+    <List>
+      <Datagrid>
+        <TextField source="borrow" />
+        <BooleanField source="returned" />
+        <BooleanField source="received" />
+        <DateField source="lent_date" />
+        <DateField source="received_date" />
+        <FunctionField label="Name" render={ConfirmButton} />
+        <FunctionField label="Name" render={DeclineButton} />
+      </Datagrid>
+    </List>
+  );
+};
