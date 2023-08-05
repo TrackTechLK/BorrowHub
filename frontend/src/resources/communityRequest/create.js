e5971702edb12bf25ad75290c297d5c385907b2b
eshans <eshans2000@gmail.com>
2023-08-05 19:44:53 +0530

    add commmunity join and accept/decline

--- frontend/src/resources/communityRequest/create.js
+++ frontend/src/resources/communityRequest/create.js
@@ -1,13 +1,22 @@
-import * as React from 'react';
-import { Create, SimpleForm, TextInput, DateInput, required ,ReferenceInput, AutocompleteInput, BooleanInput} from 'react-admin';
+import * as React from "react";
+import {
+  Create,
+  SimpleForm,
+  TextInput,
+  DateInput,
+  required,
+  ReferenceInput,
+  AutocompleteInput,
+  BooleanInput,
+} from "react-admin";
+
 
 export const CommunityRequestCreate = () => (
-    <Create>
-        <SimpleForm>
-            {/* <ReferenceInput source="user" reference='users'/> */}
-            <ReferenceInput source="community" reference='communities'>
-            <AutocompleteInput optionText="name" />
-                </ReferenceInput> 
-        </SimpleForm>
-    </Create>
-)
+  <Create>
+    <SimpleForm>
+      <ReferenceInput source="community" reference="communities">
+        <AutocompleteInput optionText="name" />
+      </ReferenceInput>
+    </SimpleForm>
+  </Create>
+);
--- frontend/src/resources/communityRequest/list.js
+++ frontend/src/resources/communityRequest/list.js
@@ -5,8 +5,50 @@ import {
     DateField,
     BooleanField,
     ReferenceField,
+    useCreate,
+    FunctionField
   } from "react-admin";
+  import Button from "@mui/material/Button";
 
+const AcceptButton = (record) => {
+  console.log(record);
+
+  const [create, { isLoading, error }] = useCreate();
+
+  const onAccept = () => {
+    console.log("update");
+    //TODO change status to accepted
+    create("user_communities", {
+      data: { user: record.user, community: record.community, is_admin: false },
+    });
+  };
+
+  return (
+    <Button variant="contained" onClick={onAccept}>
+      Accept
+    </Button>
+  );
+};
+
+
+
+  const DeclineButton = (record) => {
+    console.log(record);
+  
+    const [create, { isLoading, error }] = useCreate()
+  
+    const onDecline = () => {
+      console.log("decline");
+      //create returnconfirmation
+      // create('returnconfirmations', {data: {returned: true, received: false, borrow: record.id}})
+    };
+  
+    return (
+      <Button variant="contained" onClick={onDecline} color="error">
+        Decline
+      </Button>
+    );
+  };
   
 export const CommunityRequestList = () => (
     <List>
@@ -14,6 +56,8 @@ export const CommunityRequestList = () => (
         <TextField source="id" />
         <ReferenceField source="user" reference="users"/>
         <ReferenceField source="community" reference="communities" />
+        <FunctionField label="Name" render={AcceptButton} />
+        <FunctionField label="Name" render={DeclineButton} />
       </Datagrid>
     </List>
   );
