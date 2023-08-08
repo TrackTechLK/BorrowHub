656ced71d257055a7d394d02eb1e52bb15b49b92
eshans <eshans2000@gmail.com>
2023-08-08 19:52:22 +0530

    add community card

---
+++ frontend/src/components/CommunityCard.js
@@ -0,0 +1,43 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { Button, CardActionArea, CardActions } from "@mui/material";
+import { EditButton, ShowButton } from "react-admin";
+import { useNavigate } from "react-router-dom";
+
+export default function CommunityCard({ record }) {
+  const navigate = useNavigate();
+
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea
+        onClick={() => {
+          navigate(`/communities/${record.id}/show`);
+        }}
+      >
+        <CardMedia
+          component="img"
+          height="140"
+          //   image="url(https://source.unsplash.com/random)"
+          src="https://source.unsplash.com/random"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {record.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {record.description ||
+              "No description contains about this community"}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <ShowButton />
+        <EditButton />
+      </CardActions>
+    </Card>
+  );
+}
--- frontend/src/resources/community/list.js
+++ frontend/src/resources/community/list.js
@@ -6,15 +6,60 @@ import {
   BooleanField,
   ReferenceField,
   ShowButton,
+  WithListContext,
+  useListContext,
 } from "react-admin";
 import EfficientReferenceField from "../../components/EfficientReferenceField";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import CommunityCard from "../../components/CommunityCard";
+import { AnimatePresence, motion } from "framer-motion";
+import { WithGlow } from "../../components/WIthGlow";
+
+const CommunityListView = () => {
+  const { data } = useListContext();
+  return (
+    <AnimatePresence
+      initial={{ opacity: 0.5, scale: 0.99 }}
+      animate={{ opacity: 1, scale: 1, animation: { duration: 2000 } }}
+      exit={{ opacity: 0.5, scale: 0.99, animation: { duration: 2000 } }}
+    >
+      <Grid container style={{ padding: 10 }}>
+        {data?.map((d) => (
+          <Grid style={{ padding: 10 }} md={4} sm={6}>
+            <motion.div
+              key={d.id}
+              layout
+              transition={{ duration: 0.3 }}
+              initial={{ opacity: 0.5, scale: 0.99 }}
+              animate={{ opacity: 1, scale: 1, animation: { duration: 2000 } }}
+              exit={{
+                opacity: 0.5,
+                scale: 0.99,
+                animation: { duration: 2000 },
+              }}
+              // style={props.style}
+            >
+              <WithGlow>
+                <CommunityCard record={d} />
+              </WithGlow>
+            </motion.div>
+          </Grid>
+        ))}
+      </Grid>
+    </AnimatePresence>
+  );
+};
 
 export const CommunityList = () => (
   <List>
-    <Datagrid rowClick="show">
+    <CommunityListView />
+
+    {/* <Datagrid rowClick="show">
       <TextField source="id" />
       <TextField source="name" />
-      {/* <ReferenceField source="creator" reference="users" /> */}
+      
       <EfficientReferenceField
         label="Creator"
         reference={"users"}
@@ -22,7 +67,7 @@ export const CommunityList = () => (
         idField={"creator"}
       />
       <DateField source="created_date" locales="fr-FR" />
-      {/* <ReferenceField source="category" reference="categories" /> */}
+    
       <EfficientReferenceField
         label={"Categories"}
         reference={"categories"}
@@ -30,6 +75,6 @@ export const CommunityList = () => (
         idField={"category"}
       />
       <ShowButton />
-    </Datagrid>
+    </Datagrid> */}
   </List>
 );
