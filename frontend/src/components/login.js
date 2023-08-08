303a1befb40822b114a7f56ad6c9afab227fbc2e
kamadi2000 <kmaliyanage@gmail.com>
2023-08-08 21:49:22 +0530

    center login form

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -92,9 +92,10 @@ export default function LoginPage() {
 
   return (
     <ThemeProvider theme={theme}>
+      
       <Grid container component="main" sx={{ height: "100vh" }}>
         <CssBaseline />
-        <Grid
+        {/* <Grid
           item
           xs={false}
           sm={4}
@@ -109,8 +110,19 @@ export default function LoginPage() {
             backgroundSize: "cover",
             backgroundPosition: "center",
           }}
-        />
-        <Grid item xs={12} sm={8} md={6} component={Paper} elevation={6} square>
+        /> */}
+        <Grid item xs={12} sm={12} md={12} component={Paper} elevation={6} square>
+        <div style={{
+          backgroundImage: "url(https://source.unsplash.com/random)",
+          backgroundRepeat: "no-repeat",
+          backgroundColor: (t) =>
+            t.palette.mode === "light"
+              ? t.palette.grey[50]
+              : t.palette.grey[900],
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}>
+        
           <div
             style={{
               minHeight: "100vh",
@@ -173,6 +185,7 @@ export default function LoginPage() {
                 {/* <Copyright /> */}
               </Card>
             </div>
+          </div>
           </div>
         </Grid>
       </Grid>
