98a63c8b1224558fea890ebcf934b498a8c1b043
inuka-00 <77762018+inuka-00@users.noreply.github.com>
2023-08-10 01:34:14 +0530

    chat ui started

--- frontend/src/components/CustomAppBar.js
+++ frontend/src/components/CustomAppBar.js
@@ -2,7 +2,44 @@ import * as React from "react";
 import { AppBar, Title } from "react-admin";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
+import ChatIcon from "@mui/icons-material/Chat";
 import { WithGlow } from "./WIthGlow";
+import { IconButton } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
+
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import Messenger from "../chat/src/components/Messenger";
+import "./AppBar.css";
+const ChatButton = () => {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <>
+      <IconButton color="inherit" onClick={() => setOpen(true)}>
+        <ChatIcon fontSize="small" />
+      </IconButton>
+      <Drawer
+        className="chatbox"
+        anchor="right"
+        open={open}
+        onClose={() => setOpen(false)}
+        onOpen={() => setOpen(true)}
+      >
+        <div className="chat">
+          <Messenger />
+        </div>
+      </Drawer>
+    </>
+  );
+};
 
 export const CustomAppBar = () => (
   <AppBar>
@@ -19,5 +56,6 @@ export const CustomAppBar = () => (
       </Typography>
     </WithGlow>
     <Box flex="2" />
+    <ChatButton />
   </AppBar>
 );
